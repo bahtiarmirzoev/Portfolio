54b90eba163039a98657b769fab40b90ac4a86cb
Bakhtiyar Mirzayev <aloetopakistan@gmail.com>
2025-07-31 10:09:39 -0700

    Keeping track of the endpoints

---
+++ Movies.Api/ApiEndpoints.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
+
+namespace Movies.Api;
+
+public class ApiEndpoints
+{
+    private const string ApiBase = "api";
+
+    public static class Movies
+    {
+        private const string Base = $"{ApiBase}/movies";
+
+        public const string Create = Base;
+    }
+}
