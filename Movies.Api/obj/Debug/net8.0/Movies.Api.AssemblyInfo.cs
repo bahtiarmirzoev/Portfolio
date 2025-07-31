90b6258436f10c44e1895aac03e0c7020b3f5197
Bakhtiyar Mirzayev <aloetopakistan@gmail.com>
2025-07-31 14:23:24 -0700

    implement slug

--- Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
+++ Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f42a31cf0311603aade53b7dc74820d0d4cec96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1743692d4995befcfbc260ebd74c4b8343d506d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -14,7 +14,13 @@ public class MovieRepository: IMovieRepository
 
     public Task<Movie?> GetByIdAsync(Guid id)
     {
-        var movie = _movies.FirstOrDefault(x => x.Id == id);
+        var movie = _movies.SingleOrDefault(x => x.Id == id);
+        return Task.FromResult(movie);
+    }
+
+    public Task<Movie?> GetBySlugAsync(string slug)
+    {
+        var movie = _movies.SingleOrDefault(x => x.Slug == slug);
         return Task.FromResult(movie);
     }
 
