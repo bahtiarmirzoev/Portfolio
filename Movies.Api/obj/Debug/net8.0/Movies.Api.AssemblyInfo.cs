1601e13f1ac0f477a9eb798d62d5994f37ab37c6
Bakhtiyar Mirzayev <aloetopakistan@gmail.com>
2025-07-31 12:26:56 -0700

    implemanting movie retrival

--- Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
+++ Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b90eba163039a98657b769fab40b90ac4a86cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
