2583f5e202f28488a34def6f0000597f81671a87
Bakhtiyar Mirzayev <aloetopakistan@gmail.com>
2025-07-31 10:02:26 -0700

    introducing mapping

---
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -0,0 +1,18 @@
+using Movies.Application.Models;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Mapping;
+
+public static class ContractMapping
+{
+    public static Movie MapToMovie(this CreateMovieRequest request)
+    {
+        return new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            Year = request.Year,
+            Genres = request.Genres.ToList()
+        };
+    }
+}
