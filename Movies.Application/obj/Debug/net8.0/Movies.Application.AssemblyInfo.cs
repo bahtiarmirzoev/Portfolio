86545ad7534ba787fe8a6ad3ffabcd30d326c0d9
Bakhtiyar Mirzayev <aloetopakistan@gmail.com>
2025-08-08 15:05:04 -0700

    fix update movie endpoint

--- Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
+++ Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9514d0acc08b2b5210fec8d835d029b59078bd27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a42fe5211b380a7394157c3cc690d74951db33cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
